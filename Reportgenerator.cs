0f53540dbaf98dc61a974fec8d8d04495816e2b0
jan rahimi <rahimi.john10@gmail.com>
2024-11-22 23:50:10 +0100

    har flyttat hela reportgeneratorn metoden till klassen

--- Reportgenerator.cs
+++ Reportgenerator.cs
@@ -4,10 +4,33 @@ using InventoryManagementSystem.Models;
 namespace InventoryManagementSystem.Services
 {
     public class ReportGenerator
+{
+    public void GenerateInventoryReport(Inventory inventory)
     {
-        public void GenerateInventoryReport(Inventory inventory)
+        Console.WriteLine("\nInventory Report:");
+        foreach (var item in inventory.GetStockItems())
         {
-            // Logic for inventory reporting
+            Console.WriteLine($"{item.Product.Name}: {item.Quantity} units in stock");
         }
     }
+
+    public void GenerateLowStockReport(Inventory inventory, int threshold)
+    {
+        Console.WriteLine("\nLow Stock Report:");
+        foreach (var item in inventory.GetStockItems().Where(i => i.Quantity < threshold))
+        {
+            Console.WriteLine($"{item.Product.Name}: {item.Quantity} units in stock (below threshold of {threshold})");
+        }
+    }
+
+    public void GenerateTransactionReport(Inventory inventory)
+    {
+        Console.WriteLine("\nTransaction Report:");
+        foreach (var transaction in inventory.GetTransactions())
+        {
+            Console.WriteLine($"{transaction.Date}: {transaction.Type} - {transaction.Quantity} units of {transaction.Product.Name}");
+        }
+    }
+}
+
 }
