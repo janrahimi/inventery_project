b0288398ea134a21be428ac91790a3662f60de5a
jan rahimi <rahimi.john10@gmail.com>
2024-11-22 23:46:55 +0100

    har kommenterat lite

--- Location.cs
+++ Location.cs
@@ -1,9 +1,9 @@
 namespace InventoryManagementSystem.Models
 {
-    public class Location
+    public class Location // location klass
     {
         public int Row { get; set; }
-        public int Floor { get; set; }
+        public int Floor { get; set; } // tar hand om vart saker ligger i lagret
         public int Shelf { get; set; }
 
         public Location(int row, int floor, int shelf)
