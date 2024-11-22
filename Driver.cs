5bad80692cfd0aa211388f0e8a47d093a2c5d653
jan rahimi <rahimi.john10@gmail.com>
2024-11-22 23:28:30 +0100

    skapat driver klass

---
+++ Driver.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace InventoryManagementSystem.Models
+{
+    public class Driver
+    {
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string TruckNumber { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public Driver(string name, string company, string truckNumber, DateTime dateTime)
+        {
+            Name = name;
+            Company = company;
+            TruckNumber = truckNumber;
+            DateTime = dateTime;
+        }
+
+        public override string ToString()
+        {
+            return $"Driver: {Name}, Company: {Company}, Truck: {TruckNumber}, Date/Time: {DateTime}";
+        }
+    }
+}
