52566035ca1f99c15326e6242048dbda2316615b
jan rahimi <rahimi.john10@gmail.com>
2024-11-22 23:21:10 +0100

    har skapat supplier klass

---
+++ supplier.cs
@@ -0,0 +1,16 @@
+namespace InventoryManagementSystem.Models
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ContactInfo { get; set; }
+
+        public Supplier(int id, string name, string contactInfo)
+        {
+            Id = id;
+            Name = name;
+            ContactInfo = contactInfo;
+        }
+    }
+}
